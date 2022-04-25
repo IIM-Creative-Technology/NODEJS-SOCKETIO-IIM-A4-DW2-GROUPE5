6c5a8c860531c409b0a0e19de8a6d847a8f57d9a
julesgabriel <julesgabriel.dayaux@gmail.com>
2022-04-25 16:32:38 +0200

    ADD gitignore and routes in another folder

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use('/api', require('./api/index').router
+    // Insert here the sub route (.use like above)
+    )
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
