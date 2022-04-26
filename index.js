49c149a56bad3bced31a14d167796fba784864fa
yannyiim <yanny.ouzid@edu.devinci.fr>
2022-04-26 19:12:09 +0200

    rename app.js to index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const app = express()
+const index = express()
 const Api = require("./api/index")
 const Socket = require('./socket/index')
 const {createServer} = require("http");
 const {Server} = require('socket.io')
-const httpServer = createServer(app);
+const httpServer = createServer(index);
 const io = new Server(httpServer)
 const port = process.env.PORT || 3000
 
@@ -12,7 +12,7 @@ io.on('connection', (socket) => {
     console.log(`Connecté au client ${socket.id}`)
 })
 
-app.use('/api', Api.router
+index.use('/api', Api.router
     // Insert here the sub route (.use like above)
 ).use('/socket', Socket.socket)
 
