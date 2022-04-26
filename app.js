2e46217f0f77bd4a2575cb46bd39315cafc1092d
julesgabriel <julesgabriel.dayaux@gmail.com>
2022-04-26 15:13:50 +0200

    Update socket path for routing

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 const express = require('express')
 const app = express()
 const Api = require("./api/index")
-const { createServer } = require("http");
+const Socket = require('./socket/index')
+const {createServer} = require("http");
 const {Server} = require('socket.io')
 const httpServer = createServer(app);
 const io = new Server(httpServer)
 
-io.on('connection', (socket) =>{
+io.on('connection', (socket) => {
     console.log(`Connecté au client ${socket.id}`)
 })
 
 app.use('/api', Api.router
     // Insert here the sub route (.use like above)
-)
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+).use('/socket', Socket.socket)
 
 httpServer.listen(3000)
 
