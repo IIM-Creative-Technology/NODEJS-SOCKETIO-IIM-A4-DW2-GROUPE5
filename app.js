750d4c16532ca630fd3d06903f6e2ffcf4271934
julesgabriel <49538830+julesgabriel@users.noreply.github.com>
2022-04-26 15:19:18 +0200

    Socketio setup (#6)
    * ADD serverside
    
    * ADD client side socketio
    
    * Update socket path for routing

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const app = express()
 const Api = require("./api/index")
-const port = 3000
+const Socket = require('./socket/index')
+const {createServer} = require("http");
+const {Server} = require('socket.io')
+const httpServer = createServer(app);
+const io = new Server(httpServer)
+
+io.on('connection', (socket) => {
+    console.log(`Connecté au client ${socket.id}`)
+})
 
 app.use('/api', Api.router
     // Insert here the sub route (.use like above)
-)
+).use('/socket', Socket.socket)
 
+httpServer.listen(3000)
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
