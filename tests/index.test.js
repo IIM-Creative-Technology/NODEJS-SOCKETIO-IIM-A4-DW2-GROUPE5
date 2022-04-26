654a9b3ac26aa2220aec60bec99ae21332ee31ee
YannyOuzid <55101971+YannyOuzid@users.noreply.github.com>
2022-04-26 09:51:19 +0200

    Init jest (#2)
    * install jest with his collect coverage
    
    * add jest dependencies for express
    
    * add first test
    
    * add code coverage output in gitignore
    
    * rename jest tests

---
+++ tests/index.test.js
@@ -0,0 +1,21 @@
+const request = require("supertest");
+const index = require("../api/index");
+const express = require("express")
+const app = new express();
+app.use('/', index.router);
+
+describe("Test the root path", () => {
+
+  test("Expect OK status code from GET '/' ", () => {
+    request(index.router)
+    .get("/")
+    .expect(304)
+  });
+
+  test("Expect a json response from GET '/'", async() => {
+    const res = await request(app).get('/');
+    expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toEqual("{\"message\":\"Bienvenue sur l'api\"}");
+  });
+})
