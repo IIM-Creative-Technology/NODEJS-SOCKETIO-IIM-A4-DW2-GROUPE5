9a62eb06a1ee281b59d6267653e74140eecd883f
yannyiim <yanny.ouzid@edu.devinci.fr>
2022-04-26 09:40:23 +0200

    rename jest tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,13 +6,13 @@ app.use('/', index.router);
 
 describe("Test the root path", () => {
 
-  test("Get Ok code response from / get route", () => {
+  test("Expect OK status code from GET '/' ", () => {
     request(index.router)
     .get("/")
     .expect(304)
   });
 
-  test('Get respoonse messafe from / get route', async() => {
+  test("Expect a json response from GET '/'", async() => {
     const res = await request(app).get('/');
     expect(res.header['content-type']).toBe('application/json; charset=utf-8');
     expect(res.statusCode).toBe(200);
