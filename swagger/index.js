b85c448cb09efd0c3ab61082a971c04777725780
Julie Souchet <jsouchet@easyvista.com>
2022-04-27 21:54:48 +0200

    feat(docs): add swagger
    Add a route `/api-docs` that displays an interface for our API
    
    closes #17

---
+++ swagger/index.js
@@ -0,0 +1,17 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'NodeJS - SocketIO - Groupe 5 > Nouilles',
+            version: '1.0.0',
+        },
+    },
+    apis: ['../routes/*.js'], // files containing annotations as above
+};
+
+const openapiSpecification = swaggerJsdoc(options);
+
+module.exports = {docsRouter: [swaggerUi.serve, swaggerUi.setup(openapiSpecification)]};
