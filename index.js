e1d28ff304ce9a6fb530d54ab79eb4a680e6ae09
Julie Souchet <jsouchet@easyvista.com>
2022-04-28 13:18:32 +0200

    feature(cors): add cors middleware

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-const http = require('http')
-const express = require('express')
-const socketIO = require('socket.io')
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+const cors = require('cors');
 
-const app = express()
-const server = http.createServer(app)
-const io = socketIO(server)
+const router = require('./routes/router');
+const messenger = require('./socket/index');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-const router = require('./routes/router')
-const messenger = require('./socket/index')
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
-//Routing
-app.use('/', router)
-    .use('/m', messenger)
+app.use(cors({ origin: '*' }));
 
-//Socket.io connection
+// Routing
+app.use('/', router);
+app.use('/m', messenger);
+
+// Socket.io connection
 io.on('connection', (socket) => {
-    console.log(`Connected client :`, socket.id)
-    io.emit('socketId', socket.id)
+  console.log(`Connected client :`, socket.id);
+  io.emit('socketId', socket.id);
 
-    socket.on('chatMessage', (data) => {       
-        console.log(`Message received by :` , data.id)
-        io.emit('chatMessage', {
-            'message' : data.content,
-            'color' : data.color
-        });
-    })
-})
+  socket.on('chatMessage', (data) => {
+    console.log(`Message received by :`, data.id);
+    io.emit('chatMessage', {
+      message: data.content,
+      color: data.color,
+    });
+  });
+});
 
 server.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`)
-})
+  console.log(`Server started on http://localhost:${port}`);
+});
